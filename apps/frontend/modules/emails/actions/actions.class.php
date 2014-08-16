ececcfa84f44453cd05ca86e05efb0ee8d278d1b
Kenn Capara <kenn@teleserv.ph>
2014-08-16 15:24:27 +0800

    Email Confirmation

---
+++ apps/frontend/modules/emails/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * emails actions.
+ *
+ * @package    devcup2014
+ * @subpackage emails
+ * @author     kncapara@gmail.com
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class emailsActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
---
+++ apps/frontend/modules/emails/templates/_confirmation.php
@@ -0,0 +1,6 @@
+Hi There,
+
+Click here to confirm your account <a href="<?php echo url_for("@confirm?emailToken=$profile{['emailToken']}"); ?>"> Confirm Now</a>
+
+
+Thank you
