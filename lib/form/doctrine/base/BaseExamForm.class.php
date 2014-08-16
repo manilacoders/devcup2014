953a870ab8d4c14bf85805ea57d15ae9ed28d867
Kenn Capara <kenn@teleserv.ph>
2014-08-16 13:10:11 +0800

    new migration 6 & 7

---
+++ lib/form/doctrine/base/BaseExamForm.class.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Exam form base class.
+ *
+ * @method Exam getObject() Returns the current form's model object
+ *
+ * @package    devcup2014
+ * @subpackage form
+ * @author     kncapara@gmail.com
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
+ */
+abstract class BaseExamForm extends BaseFormDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'id'         => new sfWidgetFormInputHidden(),
+      'name'       => new sfWidgetFormInputText(),
+      'profile_id' => new sfWidgetFormInputText(),
+      'created_at' => new sfWidgetFormDateTime(),
+      'updated_at' => new sfWidgetFormDateTime(),
+    ));
+
+    $this->setValidators(array(
+      'id'         => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'name'       => new sfValidatorString(array('max_length' => 50, 'required' => false)),
+      'profile_id' => new sfValidatorInteger(array('required' => false)),
+      'created_at' => new sfValidatorDateTime(),
+      'updated_at' => new sfValidatorDateTime(),
+    ));
+
+    $this->widgetSchema->setNameFormat('exam[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'Exam';
+  }
+
+}
