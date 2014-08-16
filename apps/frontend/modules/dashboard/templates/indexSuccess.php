6e4d7668adc187f3a86b27bae647ca551b7dc798
Phillip Villarosa <ppvillarosa@gmail.com>
2014-08-16 09:11:22 +0200

    added dashboard

---
+++ apps/frontend/modules/dashboard/templates/indexSuccess.php
@@ -0,0 +1,28 @@
+<div class="col-md-2" id="side-nav">
+	<ul class="nav nav-stacked">
+	  <li class="active" data-target-page="new">
+	    <a href="#">
+	      New Exam
+	    </a>
+	  </li>
+	  <li>
+	    <a href="#" data-target-page="manage">
+	      Manage Exams
+	    </a>
+	  </li>
+	  <li>
+	    <a href="#" data-target-page="schedules">
+	      Exam Schedules
+	    </a>
+	  </li>
+	  <li>
+	    <a href="#" data-target-page="monitoring">
+	      Exam Monitoring
+	    </a>
+	  </li>
+	</ul>
+</div>
+
+<div class="col-md-8" id="main-panel">
+	test
+</div>
--- web/js/script.js
+++ web/js/script.js
@@ -1,3 +1,7 @@
 $(function() {
-	
+	console.log('ready');
+	$('#side-nav').on('click', 'li', function(e) {
+		e.preventDefault();
+		var page = $(this).find('a').data('target-page');
+	});
 });
