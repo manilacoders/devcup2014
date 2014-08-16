02302b1cabf2ca65e99a9baf0612dd9c5025882c
Daison Pascual Carino <daison12006013@gmail.com>
2014-08-16 12:34:56 +0800

    added registration for teacher

---
+++ apps/frontend/modules/profiles/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * profiles actions.
+ *
+ * @package    devcup2014
+ * @subpackage profiles
+ * @author     kncapara@gmail.com
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class profilesActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+  }
+}
