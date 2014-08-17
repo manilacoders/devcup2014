29fbb3230fa4088a58679ec42e7a910ef7e5c953
Daison Pascual Carino <daison12006013@gmail.com>
2014-08-17 10:23:24 +0800

    my updates and new fixtures

---
+++ apps/frontend/modules/reports/actions/actions.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * reports actions.
+ *
+ * @package    devcup2014
+ * @subpackage reports
+ * @author     kncapara@gmail.com
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class reportsActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    
+  }
+
+  public function executeMostExams(sfWebRequest $request)
+  {
+    
+  }
+}
