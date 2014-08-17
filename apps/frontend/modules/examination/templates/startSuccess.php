b8761d8bbb38fbc3ec03887d77dbde3b7e62be5b
Daison Pascual Carino <daison12006013@gmail.com>
2014-08-17 17:28:01 +0800

    fixed the url form in exam compute

--- apps/frontend/modules/examination/templates/startSuccess.php
+++ apps/frontend/modules/examination/templates/startSuccess.php
@@ -1,7 +1,7 @@
 <h3 class='page-header'>Exam for <?php echo $exam['name'] ?></h3>
 <legend>Questionnaires</legend>
 <div class="col-lg-12">
-	<form class="form-horizontal" role="form" action="/examination/compute" method='post'>
+	<form class="form-horizontal" role="form" action="<?php echo url_for('examination/compute') ?>" method='post'>
     <input type="hidden" name="exam_id" value="<?php echo $exam['id'] ?>">
 		<?php foreach ($questions as $question): ?>
     	<div class="panel panel-warning">
