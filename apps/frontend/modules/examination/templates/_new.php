75b98a45d69184928d53f4503c499cb3a526ceb9
Phillip Villarosa <ppvillarosa@gmail.com>
2014-08-17 11:22:15 +0200

    fixed minor ui changes

--- apps/frontend/modules/examination/templates/_new.php
+++ apps/frontend/modules/examination/templates/_new.php
@@ -1,42 +1,44 @@
 <form action="examination/createNew" method="POST" class="form-horizontal" role="form">
 	<div class="create-new">
 		<div class="row">
-			<legend>Exam Details</legend>
-			<div class="well">
-				<div class="new-exam">
-					<div class="form-group">
-						<label class="control-label col-md-2">Exam Name</label>
-						<div class="col-md-6">
-							<input type="text" class="form-control" placeholder="Exam Name" name="exam_name">
+			<div class="col-md-12">
+				<legend>Exam Details</legend>
+				<div class="well">
+					<div class="new-exam">
+						<div class="form-group">
+							<label class="control-label col-md-2">Exam Name</label>
+							<div class="col-md-6">
+								<input type="text" class="form-control" placeholder="Exam Name" name="exam_name">
+							</div>
 						</div>
-					</div>
-					
-					<div class="form-group">
-						<label class="control-label col-md-2">Active At</label>
-						<div class="col-md-6">
-							<input type="text" class="form-control datepicker" placeholder="Active At" name="active_at">
+						
+						<div class="form-group">
+							<label class="control-label col-md-2">Active At</label>
+							<div class="col-md-6">
+								<input type="text" class="form-control datepicker" placeholder="Active At" name="active_at">
+							</div>
 						</div>
-					</div>
-					
-					<div class="form-group">
-						<label class="control-label col-md-2">End At</label>
-						<div class="col-md-6">
-							<input type="text" class="form-control datepicker" placeholder="End At" name="end_at">
+						
+						<div class="form-group">
+							<label class="control-label col-md-2">End At</label>
+							<div class="col-md-6">
+								<input type="text" class="form-control datepicker" placeholder="End At" name="end_at">
+							</div>
 						</div>
-					</div>
 
-					<div class="form-group">
-						<label class="control-label col-md-2">Subject</label>
-						<div class="col-md-6">
-							<select name="subject_id" class="form-control">
-								<option value="">-- Select Subject --</option>
-								<?php foreach ($subjects as $subject): ?>
-									<option value="<?php echo $subject['id'] ?>"><?php echo $subject['name'] ?></option>
-								<?php endforeach ?>
-							</select>
+						<div class="form-group">
+							<label class="control-label col-md-2">Subject</label>
+							<div class="col-md-6">
+								<select name="subject_id" class="form-control">
+									<option value="">-- Select Subject --</option>
+									<?php foreach ($subjects as $subject): ?>
+										<option value="<?php echo $subject['id'] ?>"><?php echo $subject['name'] ?></option>
+									<?php endforeach ?>
+								</select>
+							</div>
 						</div>
-					</div>
 
+					</div>
 				</div>
 			</div>
 		</div>
