e7f17dc3c6ecf597f3c98f16926764889bab26fd
Daison Pascual Carino <daison12006013@gmail.com>
2014-08-17 13:41:25 +0800

    more updates on reports

---
+++ apps/frontend/modules/tempReports/templates/questionResultsSuccess.php
@@ -0,0 +1,20 @@
+
+<div class="col-md-8 col-md-offset-2">
+  <a href="<?php echo url_for('tempReports/examResults') ?>" class="btn btn-xsm">Go Back</a><br>
+  <h4>Question Results</h4>
+  <table class="table">
+    <tr>
+      <td></td>
+      <td>Passed</td>
+      <td>Failed</td>
+    </tr>
+    <?php foreach($questions as $name => $array): ?>
+    <tr>
+        <th><?php echo $array['name'] ?></th>
+        <td><?php echo $array['passed'] ?></td>
+        <td><?php echo $array['failed'] ?></td>
+    </tr>
+    <?php endforeach; ?>
+  </table>
+
+</div>
